3a3ce6fde9097a33562123951682e25a02011167
cattandrej <45920569+cattandrej@users.noreply.github.com>
2023-08-07 21:48:42 +0200

    add project template

---
+++ res/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $('.menu-toggle').on('click', function() {
+        $('#menuContainer').toggleClass('menu-active');
+        $('#menuLabel, #closeLabel').toggle(); // Cambia la visibilità tra MENU e la X
+    });
+
+    $(window).on("scroll", function(){
+        var scrollDistance = $(window).scrollTop();
+        var maxOpacity = 0.8;  // imposta l'opacità massima che desideri qui
+        var activationDistance = 256;  // imposta la distanza di scroll prima che inizi la trasparenza
+
+        if (scrollDistance <= activationDistance) {
+            $('.shadow-top-bar').css('opacity', 0);
+        } else if (scrollDistance <= activationDistance * 2) {
+            var opacity = ((scrollDistance - activationDistance) / activationDistance) * maxOpacity;
+            $('.shadow-top-bar').css('opacity', opacity);
+        } else {
+            $('.shadow-top-bar').css('opacity', maxOpacity);
+        }
+    });
+});
