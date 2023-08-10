f4f25c898bedb5e48d17c4dbf56a249c802f2657
cattandrej <45920569+cattandrej@users.noreply.github.com>
2023-08-10 22:57:13 +0200

    revert to 09/08/2023

--- res/js/script.js
+++ res/js/script.js
@@ -1,158 +1,132 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
     let isMobile = $(window).width() < 768;
 
-    $(window).on('resize', function () {
-        isMobile = $(window).width() < 768;
-    });
-
-
-
-    // Funzione per impostare il testo corretto su desktop
-    function setCorrectText($title, originalText, spaceWidth, numOfCopies) {
-        var space = $('<span class="space">').css('width', spaceWidth + 'px');
-        var newText = "";
-        for (var i = 0; i < numOfCopies; i++) {
-            newText += originalText + space[0].outerHTML;
-        }
-        $title.html(newText);
-
-        return {
-            single: $title.width() / numOfCopies,
-            total: $title.width()
-        };
-    }
-
-    // Funzione per iniziare l'animazione su desktop
-    function startAnimation($title, widths, spaceWidth) {
-        var animationDuration = 7500;
-        $title.stop().animate({ "margin-left": (-2 * (widths.single) + spaceWidth) }, animationDuration, "linear", function () {
-            $title.css("margin-left", 0);
-            startAnimation($title, widths, spaceWidth); // Ricomincia l'animazione
-        });
-    }
-
     // Animazione scorrimento testo se troppo lungo da mobile / animazione testo desktop durante hover
     setTimeout(() => {
-        if (isMobile) {
-            // Scorrimento testo per mobile se il testo è troppo lungo
-            $(".project-title p").each(function () {
+        $(".project-title p").each(function() {
+
+            if (isMobile) {
+    
                 var $this = $(this);
                 var parentWidth = $this.parent().width();
                 var textWidth = $this.width();
-                var spaceWidth = 50;
-
+                var spaceWidth = 50; // Larghezza dello spazio in pixel
+                
                 if (textWidth > parentWidth) {
+                    // Genera lo spazio utilizzando un elemento <span>
                     var space = $('<span class="space">').css('width', spaceWidth + 'px');
+                    
+                    // Duplica il testo e aggiungi lo spazio tra le repliche
                     $this.html($this.text() + space[0].outerHTML + $this.text());
-
-                    // Aggiungi la classe per iniziare l'animazione CSS
-                    $this.addClass('scrolling-text');
-                }
-            });
-        } else {
-            // Animazione del testo durante hover su desktop
-            $(".project").each(function () {
-                var $project = $(this);
-                var $title = $project.find(".project-title p");
-                var originalText = $title.text();
-                var hoverTimeout;
-                var spaceWidth = 100;
-                var numOfCopies = 10;
-
-                $project.hover(
-                    function () {
-                        var widths = setCorrectText($title, originalText, spaceWidth, numOfCopies);
-                        hoverTimeout = setTimeout(() => startAnimation($title, widths, spaceWidth), 500);
-                    },
-                    function () {
-                        clearTimeout(hoverTimeout);
-                        $title.stop().css("margin-left", 0).text(originalText);
+    
+                    function scrollText() {
+                        // Scorrimento del testo fino alla metà della sua lunghezza complessiva
+                        $this.animate({ "margin-left": -(textWidth + spaceWidth) }, 15000, "linear", function() {
+                            $this.css("margin-left", 0); // Resetta la posizione senza soluzione di continuità
+                            scrollText(); // Ricomincia l'animazione
+                        });
                     }
-                );
-            });
-        }
+    
+                    scrollText();
+                }
+    
+            }  else {
+                $(".project").each(function() {
+                    var $project = $(this);
+                    var $title = $project.find(".project-title p");
+                    var originalText = $title.text();
+                    var hoverTimeout;
+                    var spaceWidth = 100; // Larghezza dello spazio in pixel
+                    var numOfCopies = 10; // numero fisso di duplicati
+                
+                    var setCorrectText = function() {
+                        // Genera lo spazio utilizzando un elemento <span>
+                        var space = $('<span class="space">').css('width', spaceWidth + 'px');
+                
+                        // Duplica il testo il numero desiderato di volte con lo spazio tra le repliche
+                        var newText = "";
+                        for (var i = 0; i < numOfCopies; i++) {
+                            newText += originalText + space[0].outerHTML;
+                        }
+                        $title.html(newText);
+                        
+                        return {
+                            single: $title.width() / numOfCopies,
+                            total: $title.width()
+                        };
+                    };
+                
+                    var startAnimation = function(widths) {
+                        // Animiamo solo sulla larghezza del testo originale più lo spazio
+                        var animationDuration = 7500;
+                        $title.stop().animate({ "margin-left": (-2 * (widths.single) + spaceWidth) }, animationDuration, "linear", function() {
+                            $title.css("margin-left", 0);
+                            startAnimation(widths);
+                        });
+                    };
+                
+                    $project.hover(
+                        function() { // Mouse enter
+                            var widths = setCorrectText();
+                            hoverTimeout = setTimeout(() => startAnimation(widths), 500);
+                        },
+                        function() { // Mouse leave
+                            clearTimeout(hoverTimeout);
+                            $title.stop().css("margin-left", 0).text(originalText);
+                        }
+                    );
+                });
+            }
+        });
     }, 100);
 
 
-
-
-
     /* COMPORTAMENTO MOBILE DELL'HOVER */
-    var $projects = $('.project');
-    // Variabile globale
-    let g = [];
 
-    if (isMobile) {
-        // Inizializza g con gli elementi .project
-        $projects.each((index, project) => {
-            g.push([
-                $(project),                  // Elemento jQuery
-                false,                       // Visibile (inizialmente impostato su false)
-                !$(project).hasClass('mobile-inactive') // Attivo (true se non ha la classe 'mobile-inactive')
-            ]);
-        });
-
-        // Funzione per aggiornare l'elemento attivo basandosi su g
-        function updateActiveElement() {
-            let firstVisibleFound = false;
-
-            g.forEach((item, index) => {
-                if (item[1] && !firstVisibleFound) {
-                    // Se l'elemento è visibile e non abbiamo ancora trovato un elemento visibile
-                    if (!item[2]) {
-                        // Se l'elemento non è attivo, attivalo
-                        item[0].removeClass('mobile-inactive');
-                        g[index][2] = true;
+    $(window).on('scroll', function() {
+        if (isMobile) {
+            let noEffectElement = $('.project:not(.mobile-inactive)'); // l'elemento attuale senza l'effetto
+    
+            // Se non esiste alcun elemento senza l'effetto
+            if (noEffectElement.length === 0) {
+                $('.project').each(function() {
+                    var windowTop = $(window).scrollTop();
+                    var windowHeight = $(window).height();
+                    var elementTop = $(this).offset().top;
+                    var elementBottom = elementTop + $(this).outerHeight();
+    
+                    // Se l'elemento corrente soddisfa le condizioni
+                    if (elementTop >= windowTop && elementBottom <= (windowTop + windowHeight)) {
+                        $(this).removeClass('mobile-inactive');
+                        return false; // interrompe il ciclo
                     }
-                    firstVisibleFound = true;
-                } else {
-                    // Se l'elemento non è il primo elemento visibile o non è visibile, disattivalo
-                    if (item[2]) {
-                        item[0].addClass('mobile-inactive');
-                        g[index][2] = false;
-                    }
+                });
+            } 
+            // Se esiste un elemento senza l'effetto
+            else {
+                var windowTop = $(window).scrollTop();
+                var windowHeight = $(window).height();
+                var noEffectElementTop = noEffectElement.offset().top;
+                var noEffectElementBottom = noEffectElementTop + noEffectElement.outerHeight();
+    
+                // Se l'elemento attuale senza l'effetto non soddisfa più le condizioni
+                if (noEffectElementTop < windowTop || noEffectElementBottom > (windowTop + windowHeight)) {
+                    noEffectElement.addClass('mobile-inactive');
+    
+                    // Ciclo per trovare un altro elemento da cui rimuovere l'effetto
+                    $('.project').each(function() {
+                        var elementTop = $(this).offset().top;
+                        var elementBottom = elementTop + $(this).outerHeight();
+    
+                        if (elementTop >= windowTop && elementBottom <= (windowTop + windowHeight)) {
+                            $(this).removeClass('mobile-inactive');
+                            return false; // interrompe il ciclo
+                        }
+                    });
                 }
-            });
-        }
-
-        // Callback per IntersectionObserver
-        const handleIntersection = (entries, observer) => {
-            let visibilityChanged = false;
-
-            entries.forEach(entry => {
-                const elementIndex = g.findIndex(item => item[0][0] === entry.target);
-
-                if (entry.isIntersecting && !g[elementIndex][1]) {
-                    g[elementIndex][1] = true;
-                    visibilityChanged = true;
-                } else if (!entry.isIntersecting && g[elementIndex][1]) {
-                    g[elementIndex][1] = false;
-                    visibilityChanged = true;
-                }
-            });
-
-            // Aggiorna l'elemento attivo solo se c'è stato un cambiamento nella visibilità
-            if (visibilityChanged) {
-                updateActiveElement();
             }
-        };
-
-        // Opzioni per l'IntersectionObserver
-        const options = {
-            root: null, // viewport
-            rootMargin: '0px',
-            threshold: 0.95
-        };
-
-        const observer = new IntersectionObserver(handleIntersection, options);
-
-        // Monitora ogni elemento .project
-        $projects.each((index, project) => {
-            observer.observe(project);
-        });
-    }
-
-
-
+        }
+    });
+    
 });
